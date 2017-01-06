14a02dc221490fe89df1476b7d9c77e3355e863e
jblumstein <jblumst@us.ibm.com>
2017-01-06 10:36:37 -0500

    add tagging, combine functions to get blog post attributes, update templates

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 ### posts not to index in list of blog posts
-not_blog_posts = set(['index.html', 'base.html', 'hello.html', 'old-website-build.html', 'blog-post-template.html'])
+not_blog_posts = set(['index.html', 'base.html', 'hello.html', 'old-website-build.html', 'blog-post-template.html', 'index-with-tagged-posts.html', 'main-page-base.html'])
 
 
 ### start functions for displaying blog post information ###
@@ -36,11 +36,14 @@ def get_post_data(folder, post):
 	stub = post[:-5]
 	url_path = '/blog/' + str(stub)
 	printed_article = print_article(folder, post)
-	title = get_article_title(printed_article)
-	date = get_article_date(printed_article)
+	title = get_article_attribute(printed_article, 'title', 'Untitled')
+	date = get_article_attribute(printed_article, 'date', '2015-01-01')
 	date = datetime.strptime(date, '%Y-%m-%d').date()
-	description = get_article_description(printed_article)
-	post_data = {'post':post, 'stub':stub, 'url_path':url_path, 'title': title, 'date': date, 'description': description}
+	description = get_article_attribute(printed_article, 'description', 'No description')
+	tags = get_article_attribute(printed_article, 'tags', '')
+	tags = tags.split(sep=', ')
+	tags = set(tags)
+	post_data = {'post':post, 'stub':stub, 'url_path':url_path, 'title': title, 'date': date, 'description': description, 'tags': tags}
 	return post_data
 
 def print_article(folder, article):
@@ -50,29 +53,14 @@ def print_article(folder, article):
 		printed_article=myfile.read()
 		return printed_article
 
-def get_article_title(article_printout):
-	'''get article title from article as string'''
+def get_article_attribute(article_printout, attribute, fallback):
+	'''get article article from article as string'''
+	search_string = '\{% block ' + attribute + ' %}(.+?)\{% endblock %}.*?'
 	try:
-		article_title = re.findall(r'\{% block title %}(.+?)\{% endblock %}.*?',article_printout)[0]
+		article_attribute = re.findall(search_string, article_printout)[0]
 	except:
-		article_title = ""
-	return article_title
-
-def get_article_date(article_printout):
-	'''get article date from article as string'''
-	try:
-		article_date = re.findall(r'\<h2 id="date">(.+?)\</h2>.*?',article_printout)[0]
-	except:
-		article_date = "2015-01-01"
-	return article_date	
-
-def get_article_description(article_printout):
-	'''get article description from article as string'''
-	try:
-		article_description = re.findall(r'\{% block description %}(.+?)\{% endblock %}.*?',article_printout)[0]
-	except:
-		article_description = ""
-	return article_description
+		article_attribute = fallback
+	return article_attribute
 
 ### end functions for displaying blog post information ###
 
@@ -97,3 +85,9 @@ def render_one_post(blog_post_short_name):
 	'''render blog posts'''
 	if blog_post_short_name + '.html' not in not_blog_posts:
 		return render_template('%s.html' % blog_post_short_name)
+
+@app.route("/blog_posts_tagged:<string:tag>/")
+def render_list_of_tagged_posts(tag):
+	'''render index page with posts with desired tag'''
+	tag_with_spaces = tag.replace('+',' ')
+	return render_template('index-with-tagged-posts.html', blog_posts=blog_posts_and_paths, tag=tag)
