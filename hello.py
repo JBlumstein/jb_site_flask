4432b764f4292ce946151ff941e5209e273f0d94
Jonah Blumstein <jonah.blumstein@gmail.com>
2022-06-01 22:17:03 -0400

    updates

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 ### posts not to index in list of blog posts
-not_blog_posts = set(['index.html', 'base.html', 'hello.html', 'old-website-build.html', 'blog-post-template.html', 'index-with-tagged-posts.html', 'main-page-base.html'])
+not_blog_posts = set(['index.html', 'base.html', 'hello.html', 'old-website-build.html', 'blog-post-template.html', 'index-with-tagged-posts.html', 'main-page-base.html', 'lords-of-strategy-review.html'])
 
 
 ### start functions for displaying blog post information ###
