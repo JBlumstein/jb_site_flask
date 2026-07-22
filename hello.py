88fb15dc886adfab37fb3f5e9f013b2873a42047
Jonah Blumstein <jonah.blumstein@gmail.com>
2026-07-21 22:03:41 -0400

    allow cross site imgs + fonts

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 from flask_talisman import Talisman
 
 app = Flask(__name__)
-Talisman(app)
+Talisman(app, content_security_policy={})
 
 ### posts not to index in list of blog posts
 not_blog_posts = set(['index.html', 'base.html', 'hello.html', 'old-website-build.html', 'blog-post-template.html', 'index-with-tagged-posts.html', 'main-page-base.html', 'lords-of-strategy-review.html'])
